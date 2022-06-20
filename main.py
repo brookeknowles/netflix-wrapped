7f13bcca90934417670c057987eed91fbc033e64
Brooke Knowles <brookeknowles7@gmail.com>
2022-06-20 22:35:12 +1200

    most popular titles

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def get_users():
 
 
 def convert_string_to_seconds(s):
-    """ Helper function for get_viewing_time(). Converts a time in the format 'HH:MM:SS' to seconds float """
+    """ Helper function for get_watch_time(). Converts a time in the format 'HH:MM:SS' to seconds float """
     hours, minutes, seconds = [float(x) for x in s.split(':')]
     return (hours * 3600) + (minutes * 60) + seconds
 
@@ -96,3 +96,78 @@ def get_money_paid():
     plt.show()                                      # Display
 
     return "{:.2f}".format(money_paid)              # Convert to 2dp like actual money
+
+
+def is_tv_show(title):
+    """ Returns a boolean true if the title is a TV show, false if it's a movie"""
+    if "Episode" in title:
+        return True
+    return False
+
+
+def strip_tv_show(title):
+    """ Strips the title of a tv show from the format Show : Season (Episode) to just the show.
+        e.g. 'Parks and Recreation: Season 1: Pilot (Episode 1)' becomes 'Parks and Recreation' """
+    if "Season" in title:
+        split_string = title.split(": Season")
+    elif "Collection" in title:
+        split_string = title.split(": Collection")
+    elif "Limited Series" in title:
+        split_string = title.split(": Limited Series")
+    elif "Series" in title:
+        split_string = title.split(": Series")
+    else:
+        return title
+    return split_string[0]
+
+
+def get_most_common_title():
+    """ Finds the 10 most popular TV Shows from their titles.
+        Note this could technically include movies/non series too, but would be fairly unlikely as
+        you'd have to watch alot of times for it to beat the series' to the top 10"""
+    df = pd.read_csv(filepath + '/CONTENT_INTERACTION/ViewingActivity.csv')
+
+    for i in df.index:
+        if is_tv_show(df['Title'][i]):
+            old_title = df['Title'][i]
+            new_title = strip_tv_show(df['Title'][i])
+            df['Title'] = df['Title'].replace([old_title], new_title)
+        if df['Title'][i] == "NullCompleteVideo":
+            df = df.drop(index=i)
+
+    n = 10                                                                      # number of titles to include on graph
+    most_common_titles = df['Title'].value_counts().index.tolist()[:n]
+    most_common_values = df['Title'].value_counts().values.tolist()[:n]
+
+    plt.figure(figsize=(15, 5))
+    plt.bar(most_common_titles, most_common_values, color='red', width=0.4)     # Plot
+    plt.xlabel("Series")                                                        # Labelling plot
+    plt.ylabel("No. of times watched")
+    plt.title("Most popular TV series")
+    plt.tight_layout()                                                          # adjust figure area so labels fit nice
+    plt.show()                                                                  # Display
+
+
+def get_most_common_movies():
+    """ Finds the most popular movies/non series from their titles
+
+    TODO: fix plots so that long movie titles dont get all jumbled up
+    """
+    df = pd.read_csv(filepath + '/CONTENT_INTERACTION/ViewingActivity.csv')
+
+    for i in df.index:
+        if is_tv_show(df['Title'][i]) or df['Title'][i] == "NullCompleteVideo":
+            df = df.drop(index=i)
+
+    n = 10                                                                      # number of titles to include on graph
+    most_common_titles = df['Title'].value_counts().index.tolist()[:n]
+    most_common_values = df['Title'].value_counts().values.tolist()[:n]
+
+    plt.figure(figsize=(15, 5))
+    plt.bar(most_common_titles, most_common_values, color='red', width=0.4)     # Plot
+    plt.xlabel("Movie")                                                         # Labelling plot
+    plt.ylabel("No. of times watched")
+    plt.title("Most popular movies")
+    plt.locator_params(axis="y", integer=True, tight=True)                      # Force Y Axis to be integer values only
+    plt.tight_layout()                                                          # adjust figure area so labels fit nice
+    plt.show()                                                                  # Display
