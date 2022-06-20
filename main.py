315949619657f40f2bf0f4b895020fd657b30f82
Brooke Knowles <brookeknowles7@gmail.com>
2022-06-20 20:47:35 +1200

    most watched days of week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 filepath = '/Users/brookeknowles/Downloads/netflix-report'
 
@@ -13,11 +14,12 @@ def get_users():
 
     return users
 
+
 def convert_string_to_seconds(s):
     """ Helper function for get_viewing_time(). Converts a time in the format 'HH:MM:SS' to seconds float """
     hours, minutes, seconds = [float(x) for x in s.split(':')]
-
     return (hours * 3600) + (minutes * 60) + seconds
+
 
 def get_watch_time():
     """ Finds the total watch time for each user """
@@ -34,11 +36,37 @@ def get_watch_time():
                 current_user_time += convert_string_to_seconds(df['Duration'][i])
         users_watch_time.append(current_user_time)
 
-    plt.pie(users_watch_time, labels=users)       # Plot
+    plt.pie(users_watch_time, labels=users)         # Plot
     plt.title("Users total watch time")             # Labelling plot
     plt.show()                                      # Display
 
     return users_watch_time
+
+
+def convert_date_string_to_weekday(s):
+    """ Converts a string in the form YYYY-MM-DD HH:MM:SS to an int representing a weekday.
+        0 = Monday, 1 = Tuesday, 2 = Wednesday, 3 = Thursday, 4 = Friday, 5 = Saturday, 6 = Sunday"""
+    dt = datetime.fromisoformat(s)
+    return dt.weekday()
+
+
+def get_most_watched_days_of_week():
+    """ Finds which days of the week have the most Netflix activity """
+
+    df = pd.read_csv(filepath + '/CONTENT_INTERACTION/ViewingActivity.csv')
+
+    activity = [0, 0, 0, 0, 0, 0, 0]        # list that will be indexed like 0 = monday activity to 6 = sunday activity
+    for i in df.index:
+        activity_day = convert_date_string_to_weekday(df['Start Time'][i])
+        activity[activity_day] += 1         # if there's activity on a day, increment activity for that day
+
+    days = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
+
+    plt.bar(days, activity, color='red', width=0.4)     # Plot
+    plt.xlabel("Day of week")                           # Labelling plot
+    plt.ylabel("No. of shows/movies watched")
+    plt.title("Netflix activity by day of week")
+    plt.show()                                          # Display
 
 
 def get_money_paid():
@@ -62,12 +90,9 @@ def get_money_paid():
     df = df.set_index("Transaction Date")                                   # Setting the Date as index
 
     plt.plot(df["Gross Sale Amt"], marker='o')      # Plot
-
-    # Labelling plot
-    plt.xlabel("Date")
+    plt.xlabel("Date")                              # Labelling plot
     plt.ylabel("Cost for a month of Netflix")
     plt.title("Netflix cost over time")
-
     plt.show()                                      # Display
 
     return "{:.2f}".format(money_paid)              # Convert to 2dp like actual money
